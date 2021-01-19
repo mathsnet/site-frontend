9d6c3a79122f5f40f6ea1e438ef63138a2d8a7b7
Rasta <xarmzon@gmail.com>
2021-01-19 22:37:39 +0000

    update to frontend 19th Jan. 2021 - 3

--- pages/auth/recovery.vue
+++ pages/auth/recovery.vue
@@ -3,8 +3,21 @@
 </template>
 
 <script>
+import { CONSTANTS } from '~/assets/javascript/constants'
+
 export default {
   name: 'Recovery',
+  layout: 'auth',
+  head: {
+    title: `Forget Password`,
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: `${CONSTANTS.APP_NAME} account recovery page, Online maths schools`,
+      },
+    ],
+  },
 }
 </script>
 
